e41b2e99a8797d010a6ff7fa2e6a8028ea3d19b1
Malita Alin <105451917+Not-A-Platypus@users.noreply.github.com>
2023-05-20 15:08:14 +0300

    Indentation change

--- javascript.js
+++ javascript.js
@@ -9,32 +9,28 @@ function openModal() {
   function closeModal() {
     $("#myModal").css("display","none");
   }
-
- $(document).on('keydown', function(event) {
-       if (event.key == "Escape") {
-         closeModal();
-       }
-   });
     
   var slideIndex = 1;
   showSlides(slideIndex);
-  
-  // Next/previous controls
+  // Next&previous controls
   function plusSlides(n) {
     showSlides(slideIndex += n);
   }
   
-  var locked = false;
+  $(document).on('keydown', function(event) {
+    if (event.keyCode == 27) {
+      closeModal();
+    }
+});
+
   $(document).on('keyup', function(event) {
- 
     if (event.keyCode == '39') {
       plusSlides(1); //next - right arrow key
     }
     else if (event.keyCode == '37') {
       plusSlides(-1); //prev- leftarrow key
     } 
-
-});
+  });
 
   // Thumbnail image controls
   function currentSlide(n) {
@@ -42,8 +38,8 @@ function openModal() {
   }
   
 
-
-
+  var locked = false;
+ 
   function showSlides(n) {
     var artgallery=$(".gallery-item");
     var slides =$(".mySlides");
@@ -52,8 +48,8 @@ function openModal() {
     var captions = $(".captions ");
     var captionText = $("#slidecaption");
 
-    if (n > artgallery.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = artgallery.length}
+    if (n > artgallery.length) {slideIndex = 1;}
+    if (n < 1)                 {slideIndex = artgallery.length;}
 
       slides.css("display","none");
       thumbnails.removeClass("active");
@@ -61,34 +57,30 @@ function openModal() {
       var targetart="image/art/img"+slideIndex+".jpg";
       $(".slideImg").attr("src",targetart); 
 
-      
-
-      slides.fadeToggle(500);//fades-in slide
-    //sets slideImd to correspondent image from gallery
+    slides.fadeToggle(500);//fades-in slide
     slides.css("display","block");//slide is made visible
-    
 
     var thumbIndex=0; var x =0;   
-for(var i=-2; i<=2; i++){ //for loop assigns the thumbnails under each Slide, so that the thumbnails are the 2 previous art images + the next 2 art images
-  if(i!=0){ //skips i==0 so not to include the Slide image
-  if(slideIndex==1 && i<0 || slideIndex==2 && i<-1 ){ // exception for the first two elements
-    thumbIndex=artgallery.length+slideIndex+i;
-   
-  }else if(slideIndex == artgallery.length && i>0 || slideIndex == artgallery.length-1 && i>1 ){// exception for the last two elements
-    thumbIndex=slideIndex-artgallery.length+i;
-  }
-  else{ 
-    thumbIndex=slideIndex+i; //general case
-  }  //console.log("i="+i+" thumbIndex="+thumbIndex+" x="+x);
-    thumbnails.eq(x).attr("src","image/art/img"+thumbIndex+".jpg");
-    thumbnails.eq(x).attr("onclick","currentSlide("+thumbIndex+")"); //edits one by one the thumbnails image source and onclick at the bottom
-    x++;
-  } }
-
-    thumbnails.eq(slideIndex-1).addClass("active"); // 
-    slidenr.text(slideIndex+"/"+artgallery.length); //slides are numbered automatically
-    captionText.html(captions.eq(slideIndex-1).html());
-    captionText.addClass(".captions"); //takes the hidden captions and adds them on the slide
+   for(var i=-2; i<=2; i++){ //for loop assigns the thumbnails under each Slide, so that the thumbnails are the 2 previous art images + the next 2 art images
+    if(i!=0){ //skips i==0 so not to include the Slide image
+      if(slideIndex==1 && i<0 || slideIndex==2 && i<-1 ){ // exception for the first two elements
+         thumbIndex=artgallery.length+slideIndex+i; 
+        }
+      else if(slideIndex == artgallery.length && i>0 || slideIndex == artgallery.length-1 && i>1 ){// exception for the last two elements
+         thumbIndex=slideIndex-artgallery.length+i;
+        }
+      else{ 
+         thumbIndex=slideIndex+i; //general case
+        }  //console.log("i="+i+" thumbIndex="+thumbIndex+" x="+x);
+     thumbnails.eq(x).attr("src","image/art/img"+thumbIndex+".jpg");
+     thumbnails.eq(x).attr("onclick","currentSlide("+thumbIndex+")"); //edits one by one the thumbnails image source and onclick at the bottom
+     x++;
+   }}
+    
+   thumbnails.eq(slideIndex-1).addClass("active"); // 
+   slidenr.text(slideIndex+"/"+artgallery.length); //slides are numbered automatically
+   captionText.html(captions.eq(slideIndex-1).html());
+   captionText.addClass(".captions"); //takes the hidden captions and adds them on the slide
   }
   //art gallery functions -- END
  
